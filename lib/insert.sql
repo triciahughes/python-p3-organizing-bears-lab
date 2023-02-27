eb00f27217202826da1f00c8c0a0bcad5a8e66e7
triciahughes <hughes.a.tricia@gmail.com>
2023-02-27 15:10:18 -0800

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 1, "M", "Brown", "Playful", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 2, "M", "Brown", "Playful", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 1, "F", "Brown", "Playful", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 4, "M", "Brown", "Playful", 1);
+INSERT INTO bears VALUES (5,"Melissa", 5, "F", "Brown", "Playful", 1);
+INSERT INTO bears VALUES (6,"Grinch", 2, "M", "Green", "Playful", 1);
+INSERT INTO bears VALUES (7,"Wendy", 6, "F", "Red", "Playful", 1);
+INSERT INTO bears VALUES (8,NULL, 1, "M", "Brown", "Playful", 1);
